d129c43362945aa7a4999d95c60c3af11fd7f473
allen.phetogo <Allen.phetogo@gmail.com>
2021-06-18 00:48:15 +0200

    pages

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About This App</h1>
+      <p>React application to consume NY Times Most Popular Articles </p>
+      <p>Version: 1.0.0</p>
+    </Fragment>
+  );
+};
+
+export default About;
